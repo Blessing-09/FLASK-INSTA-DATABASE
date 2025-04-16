dce1a264beb4650d5d12de40866e4f7bd2c8c938
Blessing <blessingogbogu@gmail.com>
2025-04-16 02:25:29 +0000

    Saving functioning relationships

--- src/models.py
+++ src/models.py
@@ -1,15 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+from typing import List
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(120), unique=False, nullable=False)
+    last_name: Mapped[str] = mapped_column(String(120), unique=False, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    posts: Mapped[List["Post"]] = relationship(back_populates="user")
+    followers: Mapped[List["Follower"]] = relationship(back_populates="user")
 
     def serialize(self):
         return {
@@ -17,3 +27,15 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+class Post(db.Model):
+    __tablename__ = "post"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="posts")
+
+class Follower(db.Model):
+    __tablename__ = "follower"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_to_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="followers")
